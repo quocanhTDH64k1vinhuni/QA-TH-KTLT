403f176da7cbdf924a75b7eb30d3ad2c4af4822e
quocanhTDH64k1vinhuni <anh635808@gmail.com>
2024-11-28 13:45:05 +0700

    2.1.py

---
+++ Bài 2/2.1.py
@@ -0,0 +1,9 @@
+print("Nguyễn Quốc Anh")
+print("MSV 235752021610084")
+n1=int(input("enter n1 value"))
+n2=int(input("enter n2 value"))
+
+if n1 > n2:
+ print("n1 is big")
+else:
+ print("n2 is big")
