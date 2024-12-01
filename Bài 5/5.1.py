063d812bb78c293992b51fddc1603083ad18ac80
quocanhTDH64k1vinhuni <anh635808@gmail.com>
2024-12-01 15:06:54 +0700

    5.1.py

---
+++ Bài 5/5.1.py
@@ -0,0 +1,13 @@
+print("Nguyễn Quốc Anh")
+print("MSV 235752021610084")
+## My script using the math module ##
+import mymath # Note no .py
+values = [2,4,6,8,10]
+print('Squares:')
+for v in values:
+    print(mymath.square(v))
+print('Cubes:')
+for v in values:
+    print(mymath.cube(v))
+print('Average: ' + str(mymath.average(values)))
+
