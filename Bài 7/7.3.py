58ae4f964cec47f8b2d2a8408dcaf0a2b6f0e831
quocanhTDH64k1vinhuni <anh635808@gmail.com>
2024-12-05 14:03:54 +0700

    Add files via upload

---
+++ Bài 7/7.3.py
@@ -0,0 +1,21 @@
+print("Nguyễn Quốc Anh")
+print("MSV 235752021610084")
+def read_whole_file(filename):
+    try:
+       
+        with open(filename, 'r', encoding='utf-8') as file:
+            
+            content = file.read()
+            print("Nội dung của tệp:")
+            print(content)
+    except FileNotFoundError:
+        print(f"Không tìm thấy tệp '{filename}'.")
+    except Exception as e:
+        print(f"Có lỗi xảy ra: {e}")
+
+def main():
+    filename = input("Nhập tên tệp (bao gồm phần mở rộng, ví dụ: 'data.txt'): ")
+    read_whole_file(filename)
+
+if __name__ == "__main__":
+    main()
---
+++ Bài 7/7.4.py
@@ -0,0 +1,24 @@
+print("Nguyễn Quốc Anh")
+print("MSV 235752021610084")
+def read_first_n_lines(filename, n):
+    try:
+        with open(filename, 'r', encoding='utf-8') as file:
+            
+            for i in range(n):
+                line = file.readline()
+                if not line:  
+                    break
+                print(line.strip())  
+    except FileNotFoundError:
+        print(f"Không tìm thấy tệp '{filename}'.")
+    except Exception as e:
+        print(f"Có lỗi xảy ra: {e}")
+
+def main():
+    filename = input("Nhập tên tệp (bao gồm phần mở rộng, ví dụ: 'data.txt'): ")
+    n = int(input("Nhập số dòng bạn muốn đọc: "))
+    read_first_n_lines(filename, n)
+
+
+if __name__ == "__main__":
+    main()
